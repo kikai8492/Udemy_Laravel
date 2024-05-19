c2651c0fdc6f9c0dfe50b75d783df70e26b472c0
shuto kikai <yysskikai04@icloud.com>
2024-05-19 20:23:52 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+    // return view('welcome');
+// });
 
 Route::get('/helloworld', fn () => view('helloworld'));
 
+Route::get('/', fn() => view('index'));
+Route::get('/carriculum', fn() => view('carriculum'));
+
+// 世界の時間＝
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
