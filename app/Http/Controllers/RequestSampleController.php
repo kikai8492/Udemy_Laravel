e8d7d4009bedfdd280d68acfe8e2e9f1a0bb3a40
shuto kikai <yysskikai04@icloud.com>
2024-05-21 15:46:22 +0900

    commit

--- app/Http/Controllers/RequestSampleController.php
+++ app/Http/Controllers/RequestSampleController.php
@@ -14,4 +14,15 @@ class RequestSampleController extends Controller
         $keyword = $request -> keyword;
 				return 'キーワードは、「'.$keyword.'」です';
      }
+
+    public function profile($id)
+     {
+        return 'ID:'. $id;
+     }
+
+     public function productsArchive(Request $request, $category, $year)
+     {
+        return 'category:'.$category. '<br>year: '. $year. '<br>page:'. $request -> get(key:'page', default:1);
+     }
 }
+
