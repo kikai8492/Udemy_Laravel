162c07b9d2468bd9a8f19171d763b8386c4c7db3
shuto kikai <yysskikai04@icloud.com>
2024-05-23 16:29:42 +0900

    committ

---
+++ resources/views/photos/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default')
+@section('title','画像アップロード')
+@section('content')
+  <form action="{{route('photos.store')}}" method="post" enctype="multipart/form-data">
+    @csrf
+    <label>画像:<input type="file" name="image"></label>
+    <button type="submit">アップロード</button>
+  </form>
+@endsection
